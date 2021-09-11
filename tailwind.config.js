40ac67f33a2c343b658b419157aa3f63ccab797d
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-09-11 15:24:21 -0500

    Add styling to home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  mode: "jit",
   purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: "#6B7280"
+      }
+    },
   },
   variants: {
     extend: {},
