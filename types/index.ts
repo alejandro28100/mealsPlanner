6f567b34d2a31807bdee41f80bb77fbfa67e98b2
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-09-19 20:07:36 -0500

    Add createdAt property to Receipe Interface

--- types/index.ts
+++ types/index.ts
@@ -1,22 +1,23 @@
 export interface Ingredient {
-	id: string;
-	name: string;
-	amount: string;
-	unit: string;
+  id: string;
+  name: string;
+  amount: string;
+  unit: string;
 }
 
 export interface Receipe {
-	name: string;
-	ingredients: Ingredient[];
-	author: Author
+  name: string;
+  ingredients: Ingredient[];
+  author: Author;
+  createdAt: any;
 }
 
-export interface Author{
-	displayName: string;
-	photoURL: string;
-	uid: string;
-} 
+export interface Author {
+  displayName: string;
+  photoURL: string;
+  uid: string;
+}
 
 export interface SavedReceipe extends Receipe {
-	id: string;
+  id: string;
 }
