3f31f9794cf657a81e897185b027ac5cd0ef446d
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-09-23 13:24:26 -0500

    Add ReceipesPicker

---
+++ components/ReceipesPicker.tsx
@@ -0,0 +1,72 @@
+import { FC } from "react";
+import { RadioGroup } from "@headlessui/react";
+import { SavedReceipe } from "types/index";
+
+interface ReceipesPicker {
+  selectedReceipe: SavedReceipe;
+  receipes: SavedReceipe[];
+  handleSelectReceipe: (receipe: SavedReceipe) => void;
+  handleFetchNextReceipes: () => Promise<void>;
+  loading: boolean;
+  loadingNextReceipes: boolean;
+  lastPage: boolean;
+}
+
+const ReceipesPicker: FC<ReceipesPicker> = ({
+  handleSelectReceipe,
+  handleFetchNextReceipes,
+  receipes,
+  selectedReceipe,
+  loading,
+  loadingNextReceipes,
+  lastPage,
+}) => {
+  return (
+    <>
+      <RadioGroup value={selectedReceipe} onChange={handleSelectReceipe}>
+        <RadioGroup.Label className="sr-only">Receipes</RadioGroup.Label>
+
+        {loading ? (
+          "Cargando recetas..."
+        ) : (
+          <div className="space-y-2 py-4">
+            {receipes?.map((receipe) => {
+              const { name, id } = receipe;
+              return (
+                <RadioGroup.Option
+                  key={id}
+                  value={receipe}
+                  className={({ active, checked }) =>
+                    `${active && "ring-black ring-1"} 
+                    ${
+                      checked
+                        ? "bg-black text-white"
+                        : "hover:bg-black/80 hover:border-black/80 hover:text-white"
+                    } 
+                    bg-white border-black border rounded px-4 py-2 cursor-pointer select-none
+                    
+                    `
+                  }
+                >
+                  <div className="flex items-center justify-btween w-full">
+                    <RadioGroup.Label as="p" className="font-medium text-sm">
+                      {name}
+                    </RadioGroup.Label>
+                  </div>
+                </RadioGroup.Option>
+              );
+            })}
+          </div>
+        )}
+      </RadioGroup>
+      {!lastPage && (
+        <div className="">
+          <button disabled={loadingNextReceipes} onClick={handleFetchNextReceipes}>
+            Ver más recetas
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+export default ReceipesPicker;
