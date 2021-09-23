3c4439e8868646b41d9387cb16abcc092cbea39f
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-09-23 13:27:36 -0500

    Add Daily View

--- components/DatePicker/index.tsx
+++ components/DatePicker/index.tsx
@@ -1,24 +1,34 @@
-import React, { FC } from 'react';
-import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
+import React, { FC } from "react";
+import DatePicker, {
+  ReactDatePickerProps,
+  registerLocale,
+} from "react-datepicker";
+import es from "date-fns/locale/es";
 
-import 'react-datepicker/dist/react-datepicker.css';
+// import "react-datepicker/dist/react-datepicker.css";
 
+registerLocale("es", es);
 interface DatePickerProps extends ReactDatePickerProps<{}> {
-	selected: Date;
-	onChange: (date: Date) => void;
-	onSelect?: (date: Date) => void;
+  selected: Date;
+  onChange: (date: Date) => void;
+  onSelect?: (date: Date) => void;
 }
 
-const DatePickerFC: FC<DatePickerProps> = ({ onChange, selected, onSelect }) => {
-	return (
-		<DatePicker
-			onSelect={onSelect}
-			inline
-			showYearPicker={false}
-			selected={selected}
-			onChange={(date) => onChange(date as Date)}
-		/>
-	);
+const DatePickerFC: FC<DatePickerProps> = ({
+  onChange,
+  selected,
+  onSelect,
+}) => {
+  return (
+    <DatePicker
+      onSelect={onSelect}
+      locale="es"
+      inline
+      showYearPicker={false}
+      selected={selected}
+      onChange={(date) => onChange(date as Date)}
+    />
+  );
 };
 
 export default DatePickerFC;
--- components/Menu/DailyView.tsx
+++ components/Menu/DailyView.tsx
@@ -1,185 +1,175 @@
-import React, { useState, useEffect, Fragment, FC } from 'react';
-import Link from 'next/link';
-import { deleteField, limit, where } from '@firebase/firestore';
-import { getDocuments, updateDocument } from 'utils/firebase';
+import React, { useState, useEffect, Fragment, FC } from "react";
+import { deleteField, limit, where } from "@firebase/firestore";
+import { db, getDocuments, updateDocument } from "utils/firebase";
+import { useUser } from "hooks/userUser";
+import { subDays, addDays } from "date-fns";
+import "utils/date.ts";
 
-import { useUser } from 'hooks/userUser';
-
-import 'utils/date.ts';
+import AddReceipesModal from "components/AddReceipesModal";
 
 interface Menu {
-	id: string;
-	date: Date;
-	meals: Meal[];
+  id: string;
+  date: Date;
+  meals: Meal[];
 }
 
 interface Meal {
-	id: string;
-	name: string;
-	subType: string;
-	type: string;
+  id: string;
+  name: string;
+  subType: string;
+  type: string;
 }
 
-const DailyView: FC<{ date: Date; setDate: (date: Date) => void }> = ({ date, setDate }) => {
-	const { user } = useUser();
+type DayOperation = "prev" | "next" | "current";
+interface Day {
+  type: DayOperation;
+  date: Date;
+}
 
-	const [ days, setDays ] = useState<Date[]>(() => {
-		let current = date;
-		let previous = current.calculateNewDay(-1);
-		let next = current.calculateNewDay(1);
-		return [ previous, current, next ];
-	});
+const DailyView: FC<{ date: Date; setDate: (date: Date) => void }> = ({
+  date: currentDay,
+  setDate,
+}) => {
+  const { user } = useUser();
 
-	const [ menu, setMenu ] = useState<Menu>();
-	const [ loading, setLoading ] = useState(true);
-	const [ error, setError ] = useState<string>();
+  const [menu, setMenu] = useState<Menu | undefined>(undefined);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | undefined>(undefined);
 
-	useEffect(
-		() => {
-			(async function() {
-				function getStartingDate() {
-					return days.length ? days[1].getZeroHours() : new Date().getZeroHours();
-				}
+  const [isAddReceipeModalOpen, setIsAddReceipeModalOpen] = useState(false);
 
-				setMenu(undefined);
-				setError(undefined);
-				setLoading(true);
+  useEffect(() => {
+    (async function () {
+      function getStartingDate() {
+        return currentDay.getZeroHours();
+      }
 
-				try {
-					const snapshot = await getDocuments(
-						`users/${user?.uid}/menus`,
-						where('date', '==', getStartingDate()),
-						limit(1)
-					);
-					if (snapshot.docs.length !== 1) {
-						setLoading(false);
-						return;
-					}
-					snapshot.forEach((doc) => {
-						let meals: Meal[] = [];
-						let { date, meals: mealsMap } = doc.data();
+      setMenu(undefined);
+      setError(undefined);
+      setLoading(true);
 
-						Object.keys(mealsMap).forEach((mealID) => {
-							let meal: Meal = {
-								id: mealID,
-								...mealsMap[mealID]
-							};
-							meals.push(meal);
-						});
+      try {
+        const snapshot = await getDocuments(
+          `users/${user?.uid}/menus`,
+          where("date", "==", getStartingDate()),
+          limit(1)
+        );
+        snapshot.forEach((doc) => {
+          let meals: Meal[] = [];
+          let { date, meals: mealsMap } = doc.data();
 
-						setMenu({
-							id: doc.id,
-							date,
-							meals
-						} as Menu);
-					});
-					setLoading(false);
-				} catch (error) {
-					setError(error as string);
-				}
-			})();
-		},
-		[ days ]
-	);
+          Object.keys(mealsMap).forEach((mealID) => {
+            let meal: Meal = {
+              id: mealID,
+              ...mealsMap[mealID],
+            };
+            meals.push(meal);
+          });
 
-	function handleClick(index: number) {
-		let newCurrentDate: unknown;
-		if (index === 0) {
-			// Go to yesterday
-			setDays(([ previous, current ]) => {
-				newCurrentDate = previous;
-				return [ previous.calculateNewDay(-1), previous, current ];
-			});
-		} else if (index === 2) {
-			// Go to tomorrow
-			setDays(([ _, current, next ]) => {
-				newCurrentDate = next;
-				return [ current, next, next.calculateNewDay(1) ];
-			});
-		}
-		//Update global date
-		setDate(newCurrentDate as Date);
-		return;
-	}
+          setMenu({
+            id: doc.id,
+            date,
+            meals,
+          } as Menu);
+        });
+        setLoading(false);
+        setError(undefined);
+      } catch (error) {
+        setError(error as string);
+        setLoading(false);
+      }
+    })();
+  }, [currentDay]);
 
-	async function handleRemoveMeal(id: string) {
-		const path = `/menus/${menu?.id}`;
-		const mealPath = `meals.${id}`;
+  function handleShowDay(dayType: DayOperation) {
+    switch (dayType) {
+      case "next": {
+        const updatedDay = addDays(currentDay, 1);
+        setDate(updatedDay);
+        break;
+      }
 
-		await updateDocument(path, {
-			[mealPath]: deleteField()
-		});
+      case "prev": {
+        const updatedDay = subDays(currentDay, 1);
+        setDate(updatedDay);
+        break;
+      }
 
-		setMenu((prev) => {
-			return {
-				...prev,
-				meals: prev && prev.meals.filter((meal) => meal.id !== id)
-			} as Menu;
-		});
+      default:
+        break;
+    }
+  }
 
-		console.log('Document Updated');
-	}
+  async function handleRemoveMeal(id: string) {
+    const path = `/menus/${menu?.id}`;
+    const mealPath = `meals.${id}`;
 
-	return (
-		<section style={{ display: 'flex' }}>
-			{days.length && (
-				<Fragment>
-					<div style={{ width: '25%', background: 'lightblue', textAlign: 'center' }}>
-						<h2>Días</h2>
-						{error && <h2>Algo salió mal, inténtalo más tarde :(</h2>}
+    await updateDocument(path, {
+      [mealPath]: deleteField(),
+    });
 
-						{days.map((day, index) => (
-							<div
-								key={day.toDateString()}
-								onClick={() => handleClick(index)}
-								style={{ display: 'block', textAlign: 'center', padding: '1em', cursor: 'pointer' }}
-							>
-								{day.getDate()}
-							</div>
-						))}
-					</div>
-					<div>
-						<h2>{days[1].toSpanishDate()}</h2>
-						{menu ? (
-							<div>
-								{menu.meals.length >= 1 ? (
-									menu.meals.map(({ id, name, type, subType }) => (
-										<div key={id}>
-											<h3 style={{ display: 'inline-block' }}>
-												{`${name} | ${type} ${subType && ` | ${subType}`}`}
-											</h3>
-											<button onClick={() => handleRemoveMeal(id)}>Eliminar</button>
-										</div>
-									))
-								) : (
-									<Fragment>
-										<h3>No tienes ninguna comida planeada</h3>
-										<Link href="/recetas">
-											<a>
-												<button>Agrega comidas ahora</button>
-											</a>
-										</Link>
-									</Fragment>
-								)}
-							</div>
-						) : (
-							!error &&
-							!loading && (
-								<Fragment>
-									<h3>No tienes ninguna comida planeada</h3>
-									<Link href="/recetas">
-										<a>
-											<button>Agrega comidas ahora</button>
-										</a>
-									</Link>
-								</Fragment>
-							)
-						)}
-					</div>
-				</Fragment>
-			)}
-		</section>
-	);
+    setMenu((prev) => {
+      return {
+        ...prev,
+        meals: prev && prev.meals.filter((meal) => meal.id !== id),
+      } as Menu;
+    });
+  }
+
+  const days: Day[] = [
+    { type: "prev", date: subDays(currentDay, 1) },
+    { type: "current", date: currentDay },
+    { type: "next", date: addDays(currentDay, 1) },
+  ];
+
+  return (
+    <section style={{ display: "flex" }}>
+      <Fragment>
+        <div className="w-1/4 flex flex-col justify-evenly items-center space-y-[40px] py-7 border-r border-secondary/60">
+          {days.map(({ date, type }: Day, index) => (
+            <div
+              key={index}
+              onClick={() => handleShowDay(type)}
+              className="cursor-pointer flex items-center justify-center p-10 text-4xl font-black first-of-type:text-secondary/50 last-of-type:text-secondary/50"
+            >
+              {date.getDate()}
+            </div>
+          ))}
+        </div>
+        <div className="p-10">
+          {!loading ? (
+            <div>
+              {menu && menu.meals.length >= 1 ? (
+                menu.meals.map(({ id, name, type, subType }) => (
+                  <div key={id}>
+                    <h3 style={{ display: "inline-block" }}>
+                      {`${name} | ${type} ${subType && ` | ${subType}`}`}
+                    </h3>
+                    <button onClick={() => handleRemoveMeal(id)}>Eliminar</button>
+                  </div>
+                ))
+              ) : (
+                <Fragment>
+                  <h3 className="text-secondary font-semibold text-xl my-12">
+                    No tienes ninguna comida planeada para este día
+                  </h3>
+                  <div className="flex flex-col items-center justify-center">
+                    <AddReceipesModal
+                      isOpen={isAddReceipeModalOpen}
+                      toggleOpen={() => setIsAddReceipeModalOpen((prev) => !prev)}
+                    />
+                  </div>
+                </Fragment>
+              )}
+            </div>
+          ) : (
+            "Cargando"
+          )}
+          {!loading && error && <h2>Algo salió mal, inténtalo más tarde :(</h2>}
+        </div>
+      </Fragment>
+    </section>
+  );
 };
 
 export default DailyView;
